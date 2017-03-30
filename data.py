d9bacaffa4b5709aae94bf1de25eadc99f6e686c
Viktor Kozorez <viktorkozorez1997@gmail.com>
2017-03-31 00:18:55 +0300

    Added create and update operations, refactored db utils

--- data.py
+++ data.py
@@ -1,33 +1,54 @@
+"""TODO."""
+
 import psycopg2
 
+conn = None
+
+
 def get_items():
-    conn = psycopg2.connect(database="testtest", user="viktor", password="v1i2t3y4a5!", host="127.0.0.1", port="5432")
-
-    print("Opened database successfully")
-
+    """TODO."""
     cur = conn.cursor()
-
-    cur.execute("SELECT id, name, priority, category, is_finished from tasks")
+    cur.execute("SELECT id, name, priority, category, is_finished from tasks;")
     rows = cur.fetchall()
-
-    print("Operation done successfully")
-    conn.close()
     return rows
 
+
 def remove_item(item):
-    conn = psycopg2.connect(database="testtest", user="viktor", password="v1i2t3y4a5!", host="127.0.0.1", port="5432")
-    print("Opened database successfully")
-
+    """TODO."""
     cur = conn.cursor()
-    string = "DELETE from tasks where id = " + item + ";"
-    #print(string)
-    cur.execute(string)
+    cur.execute("DELETE from tasks where id = " + item + ";")
     conn.commit()
+    print("Number of records deleted:", cur.rowcount)
 
-    print("Total number of rows deleted :", cur.rowcount)
 
-    print("Operation done successfully")
+def change_item(text, values):
+    """TODO."""
+    cur = conn.cursor()
+    cur.execute("UPDATE tasks set name = '" + values[0] + "', priority = " + values[1] + ", category = '" + values[2] + "', is_finished = " + values[3] + " where ID = " + text + ";")
+    conn.commit()
+    print("Number of records updated:", cur.rowcount)
+
+
+def create_item(values):
+    """TODO."""
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO tasks (name, priority, category, is_finished) VALUES ('" + values[0] + "', " + values[1] + ", '" + values[2] + "', " + values[3] + ") RETURNING id;" );
+    text = cur.fetchone()[0]
+    conn.commit()
+    print("Records created successfully")
+    return text
+
+
+def initialize_db():
+    """TODO."""
+    print('INITIALIZING...')
+    global conn
+    conn = psycopg2.connect(database="testtest", user="viktor", password="v1i2t3y4a5!", host="127.0.0.1", port="5432")
+
+
+def shutdown_db():
+    """TODO."""
+    print('SHUTDOWN...')
+    global conn
     conn.close()
-
-def change_item(item):
-    print(item)
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+"""TODO."""
+
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 import data
 
+data.initialize_db()
+
 root = Tk()
+root.title("Task Manager")
 
 w, h = root.winfo_screenwidth(), root.winfo_screenheight()
 root.geometry("%dx%d+0+0" % (w, h))
@@ -14,131 +20,193 @@ w_category = int(w * 0.2)
 w_is_finished = int(w * 0.1)
 
 tree = ttk.Treeview(root, height=36, columns=('name', 'priority', 'category', 'is_finished'))
+
 tree.column('#0', width=w_id, anchor='center')
-tree.heading('#0', text='id')
+tree.heading('#0', text='Id')
 tree.column('name', width=w_name, anchor='center')
-tree.heading('name', text='name')
+tree.heading('name', text='Name')
 tree.column('priority', width=w_priority, anchor='center')
-tree.heading('priority', text='priority')
+tree.heading('priority', text='Priority')
 tree.column('category', width=w_category, anchor='center')
-tree.heading('category', text='category')
+tree.heading('category', text='Category')
 tree.column('is_finished', width=w_is_finished, anchor='center')
-tree.heading('is_finished', text='is_finished')
+tree.heading('is_finished', text='Is Finished')
 
 s = ttk.Scrollbar(root, orient=VERTICAL, command=tree.yview)
 tree.configure(yscrollcommand=s.set)
 
-###
-
-def calculate1():
-    pass
-
-mainframe = ttk.Frame(root, padding="75 0 100 150", relief=SUNKEN)
+mainframe = ttk.Frame(root, padding="25 25 100 50")
 mainframe.grid(row=0, column=2, sticky=(N, S, W, E))
 mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)
 
-feet = StringVar()
-meters = StringVar()
+nameee = StringVar()
+priorityee = StringVar()
+categoryee = StringVar()
+is_finishedee = StringVar()
 
-feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-feet_entry.grid(column=2, row=1, sticky=(W, E))
+ttk.Label(mainframe, text='Name:').grid(column=1, row=1, sticky=(W, E))
+namee = ttk.Entry(mainframe, width=20, textvariable=nameee)
+namee.grid(column=2, row=1, sticky=(W, E))
 
-ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
-ttk.Button(mainframe, text="Calculate", command=calculate1).grid(column=3, row=3, sticky=W)
+ttk.Label(mainframe, text='Priority:').grid(column=1, row=2, sticky=(W, E))
+prioritye = ttk.Entry(mainframe, width=20, textvariable=priorityee)
+prioritye.grid(column=2, row=2, sticky=(W, E))
 
-ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+ttk.Label(mainframe, text='Category:').grid(column=1, row=3, sticky=(W, E))
+categorye = ttk.Entry(mainframe, width=20, textvariable=categoryee)
+categorye.grid(column=2, row=3, sticky=(W, E))
+
+ttk.Label(mainframe, text='Is Finished:').grid(column=1, row=4, sticky=(W, E))
+is_finishede = ttk.Checkbutton(mainframe, variable=is_finishedee, onvalue='True', offvalue='False')
+is_finishede.grid(column=2, row=4, sticky=(W, E))
+
+
+def calculate1():
+    """TODO."""
+    global nameee, priorityee, categoryee, is_finishedee
+    ne = nameee.get()
+    pe = priorityee.get()
+    ce = categoryee.get()
+    ie = is_finishedee.get()
+
+    if validate():
+        item_text = tree.item(tree.selection()[0], "text")
+        item_values = (ne, pe, ce, ie)
+
+        data.change_item(str(item_text), item_values)
+
+        tree.item(tree.selection()[0], values=item_values)
+
+        nameee.set("")
+        priorityee.set("")
+        categoryee.set("")
+        is_finishedee.set("")
+
+        global but
+        but['state'] = 'disabled'
+        global bt
+        bt['state'] = 'normal'
+
+
+def cr():
+    """TODO."""
+    global nameee, priorityee, categoryee, is_finishedee
+    ne = nameee.get()
+    pe = priorityee.get()
+    ce = categoryee.get()
+    ie = is_finishedee.get()
+
+    if validate():
+        item_values = (ne, pe, ce, ie)
+
+        item_text = data.create_item(item_values)
+
+        tree.insert('', 'end', item_text, text=item_text, values=(item_values[0], item_values[1], item_values[2], item_values[3]))
+
+        nameee.set("")
+        priorityee.set("")
+        categoryee.set("")
+        is_finishedee.set("")
+
+        global but
+        but['state'] = 'disabled'
+        global bt
+        bt['state'] = 'normal'
+
+
+bt = ttk.Button(mainframe, text="Create Task", command=cr)
+bt.grid(column=1, row=5, sticky=(W, E))
+
+but = ttk.Button(mainframe, text="Change Task", command=calculate1)
+but.grid(column=2, row=5, sticky=(W, E))
+but['state'] = 'disabled'
 
 for child in mainframe.winfo_children():
-    child.grid_configure(padx=5, pady=5)
-
-feet_entry.focus()
-root.bind('<Return>', calculate1)
-
-###
-
-def calculate2():
-    pass
-
-mainframe2 = ttk.Frame(root, padding="75 0 100 150", relief=SUNKEN)
-mainframe2.grid(row=1, column=2, sticky=(N, S, W, E))
-mainframe2.columnconfigure(0, weight=1)
-mainframe2.rowconfigure(0, weight=1)
-
-feet2 = StringVar()
-meters2 = StringVar()
-
-feet_entry2 = ttk.Entry(mainframe2, width=7, textvariable=feet2)
-feet_entry2.grid(column=2, row=1, sticky=(W, E))
-
-ttk.Label(mainframe2, textvariable=meters2).grid(column=2, row=2, sticky=(N, S, W, E))
-ttk.Button(mainframe2, text="Calculate", command=calculate2).grid(column=3, row=3, sticky=W)
-
-ttk.Label(mainframe2, text="feet").grid(column=3, row=1, sticky=W)
-ttk.Label(mainframe2, text="is equivalent to").grid(column=1, row=2, sticky=E)
-ttk.Label(mainframe2, text="meters").grid(column=3, row=2, sticky=W)
-
-for child in mainframe2.winfo_children():
-    child.grid_configure(padx=5, pady=5)
-
-feet_entry2.focus()
-root.bind('<Return>', calculate2)
-
-###
+    child.grid_configure(padx=25, pady=25)
 
 tree.grid(row=0, column=0, rowspan=2)
-s.grid(row=0, column=1, rowspan=2, sticky=["WNES"])
-
-
-# Inserted at the root, program chooses id:
-#tree.insert('', 'end', 'widgets', text='Widget Tour', values=('1', '1', '1', '1'))
-# Same thing, but inserted as first child:
-#tree.insert('', 0, 'gallery', text='Applications', values=('2', '2', '2', '2'))
-
-#print(data.get_items())
+s.grid(row=0, column=1, rowspan=2, sticky=(W, N, E, S))
 
 for item in data.get_items():
     tree.insert('', 'end', item[0], text=item[0], values=(item[1], item[2], item[3], item[4]))
 
-global_item = None
-global_itemitem = None
 
 def closure():
-    data.remove_item(global_item)
-    tree.delete(global_item)
+    """TODO."""
+    item_text = tree.item(tree.selection()[0], "text")
+    data.remove_item(str(item_text))
+    tree.delete(item_text)
+
 
 def closure_Change():
-    data.change_item(global_itemitem)
-    #tree.(global_item)
+    """TODO."""
+    global but
+    but['state'] = 'normal'
+    global bt
+    bt['state'] = 'disabled'
+    #item_text = tree.item(tree.selection()[0], "text")
+    item_values = tree.item(tree.selection()[0], "values")
+    #data.change_item(item_text, item_values)
+    global nameee, priorityee, categoryee, is_finishedee
+    nameee.set(item_values[0])
+    priorityee.set(item_values[1])
+    categoryee.set(item_values[2])
+    is_finishedee.set(item_values[3])
 
-# create a popup menu
+
 menu = Menu(root, tearoff=0)
-menu.add_command(label="Remove Task", command = closure)
-menu.add_command(label="Change Task", command = closure_Change)
+menu.add_command(label="Remove Task", command=closure)
+menu.add_command(label="Change Task", command=closure_Change)
+
 
 def func(event):
-    item = tree.identify('item', event.x, event.y)
-    global global_item 
-    global_item = item
-    print(global_item)
-
-
-    #curItem = tree.focus()
-    #print(tree.item(curItem))
-    #print("you clicked on", tree.item(item, "text"))
+    """TODO."""
     popup(event, item)
+
 
 tree.bind("<3>", func)
 
+
 def popup(event, item):
+    """TODO."""
     if tree.focus():
         menu.post(event.x + 65, event.y)
 
+
 def func1(event):
+    """TODO."""
     menu.unpost()
 
+
 tree.bind("<1>", func1)
+
+
+def on_closing():
+    """TODO."""
+    if messagebox.askyesno(message='Are you sure you want to quit?', icon='question', title='Quit'):
+        data.shutdown_db()
+        root.destroy()
+
+
+root.protocol("WM_DELETE_WINDOW", on_closing)
+
+
+def validate():
+    """TODO."""
+    global nameee, priorityee, categoryee
+    ne = nameee.get()
+    pe = priorityee.get()
+    ce = categoryee.get()
+
+    if not(len(ne) > 0 and len(ne) <= 30):
+        return False
+    if not(int(pe) > 0 and int(pe) <= 10):
+        return False
+    if not(len(ce) > 0 and len(ce) <= 30):
+        return False
+
+    return True
+
 
 root.mainloop()
