4987369e5d1313b0ec8b27d047f062f77e5e92ea
Viktor Kozorez <viktorkozorez1997@gmail.com>
2017-04-02 22:47:18 +0300

    Refactoring; added docstrings; provided partial conformance to pep8 and pep257

--- db.py
+++ db.py
@@ -1,53 +1,74 @@
-"""TODO."""
+"""
+File: db.py.
+
+Module provides functions (CRUD) for interfacing with relational db server
+running PostgreSQL instance.
+"""
+
+import sys
 
 import psycopg2
 
 conn = None
 
 
-def get_tasks():
-    """TODO."""
-    cur = conn.cursor()
-    cur.execute("SELECT id, name, priority, category, is_finished from tasks;")
-    rows = cur.fetchall()
-    return rows
+def initialize_db(password):
+    """Initialize db connection with provided user password."""
+    print('INITIALIZING...')
+    global conn
+
+    try:
+        conn = psycopg2.connect(database="testtest",
+                                user="viktor",
+                                password=password,
+                                host="127.0.0.1",
+                                port="5432")
+
+    except psycopg2.OperationalError as exception:
+        print("PASSWORD IS INCORRECT. TRY AGAIN...")
+        sys.exit()
 
 
-def delete_task(item):
-    """TODO."""
-    cur = conn.cursor()
-    cur.execute("DELETE from tasks where id = " + item + ";")
-    conn.commit()
-    print("Number of records deleted:", cur.rowcount)
-
-
-def edit_task(text, values):
-    """TODO."""
-    cur = conn.cursor()
-    cur.execute("UPDATE tasks set name = '" + values[0] + "', priority = " + values[1] + ", category = '" + values[2] + "', is_finished = " + str(values[3]) + " where ID = " + text + ";")
-    conn.commit()
-    print("Number of records updated:", cur.rowcount)
+def shutdown_db():
+    """Close connection to db."""
+    print('SHUTDOWN...')
+    global conn
+    conn.close()
 
 
 def add_task(values):
-    """TODO."""
-    cur = conn.cursor()
-    cur.execute("INSERT INTO tasks (name, priority, category, is_finished) VALUES ('" + values[0] + "', " + values[1] + ", '" + values[2] + "', " + str(values[3]) + ") RETURNING id;" );
-    text = cur.fetchone()[0]
+    """Add specified task to the database.."""
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO tasks (name, priority, category, is_finished) VALUES (%s, %s, %s, %s) RETURNING id;",
+                   (values[0], values[1], values[2], values[3]))
+
+    text = cursor.fetchone()[0]
     conn.commit()
     print("Records created successfully")
     return text
 
 
-def initialize_db(password):
-    """TODO."""
-    print('INITIALIZING...')
-    global conn
-    conn = psycopg2.connect(database="testtest", user="viktor", password=password, host="127.0.0.1", port="5432")
+def get_tasks():
+    """Get all tasks from the database."""
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, name, priority, category, is_finished from tasks;")
+    rows_count = cursor.fetchall()
+    return rows_count
 
 
-def shutdown_db():
-    """TODO."""
-    print('SHUTDOWN...')
-    global conn
-    conn.close()
+def edit_task(id, values):
+    """Edit specified task in the database."""
+    cursor = conn.cursor()
+    cursor.execute("UPDATE tasks SET name = %s, priority = %s, category = %s, is_finished = %s WHERE id = %s;",
+                   (values[0], values[1], values[2], values[3], id))
+
+    conn.commit()
+    print("Number of records updated:", cursor.rowcount)
+
+
+def delete_task(id):
+    """Delete specified task from the database."""
+    cursor = conn.cursor()
+    cursor.execute("DELETE from tasks where id = %s;", (id, ))
+    conn.commit()
+    print("Number of records deleted:", cursor.rowcount)
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-"""TODO."""
+"""
+File: main.py.
 
-import sys
+Module provides GUI client for interaction with user tasks,
+stored in a relational database.
+Db interactions are controlled by module db (File: db.py) through delegation.
+"""
 
 from tkinter import *
 from tkinter import ttk
@@ -11,57 +15,56 @@ import db
 
 password_root = Tk()
 password_root.withdraw()
-password = simpledialog.askstring("Password", "Enter password:", show='*')
+password = simpledialog.askstring("Password", "Enter password:", show="*")
 password_root.destroy()
 
 if password is None:
     sys.exit()
 
-try:
-    db.initialize_db(password)
-except:
-    print("PASSWORD IS INCORRECT. TRY AGAIN...")
-    sys.exit()
+db.initialize_db(password)
 
 root = Tk()
 root.title("Task Manager")
+columns = ("name", "priority", "category", "is_finished")
+tree = ttk.Treeview(root, height=36, selectmode="browse", columns=columns, show="headings")
+scrollbar = ttk.Scrollbar(root, orient=VERTICAL, command=tree.yview)
+tree.configure(yscrollcommand=scrollbar.set)
+tree.grid(row=0, column=0, rowspan=2)
+scrollbar.grid(row=0, column=1, rowspan=2, sticky=(W, N, E, S))
+
+
+def treeview_sort_column(treeview, column, reverse):
+    """Provide sorting on treeview columns."""
+    children_list = [(treeview.set(child, column), child) for child in treeview.get_children("")]
+    children_list.sort(reverse=reverse)
+
+    for index, (value, child) in enumerate(children_list):
+        treeview.move(child, "", index)
+
+    treeview.heading(column, command=lambda: treeview_sort_column(treeview, column, not reverse))
+
+
+for column in columns:
+    tree.heading(column, text=column, command=lambda col=column: treeview_sort_column(tree, col, False))
 
 width, height = root.winfo_screenwidth(), root.winfo_screenheight()
 root.geometry("{0}x{1}+0+0".format(width, height))
 
 width_name = int(width * 0.26)
+tree.column("name", width=width_name, anchor="center")
+tree.heading("name", text="Name")
+
 width_priority = int(width * 0.06)
+tree.column("priority", width=width_priority, anchor="center")
+tree.heading("priority", text="Priority")
+
 width_category = int(width * 0.21)
+tree.column("category", width=width_category, anchor="center")
+tree.heading("category", text="Category")
+
 width_is_finished = int(width * 0.11)
-
-
-def treeview_sort_column(tv, col, reverse):
-    """TODO."""
-    l = [(tv.set(k, col), k) for k in tv.get_children('')]
-    l.sort(reverse=reverse)
-
-    for index, (val, k) in enumerate(l):
-        tv.move(k, '', index)
-
-    tv.heading(col, command=lambda: treeview_sort_column(tv, col, not reverse))
-
-
-columns = ('name', 'priority', 'category', 'is_finished')
-tree = ttk.Treeview(root, height=36, columns=columns, show="headings", selectmode="browse")
-for col in columns:
-    tree.heading(col, text=col, command=lambda _col=col: treeview_sort_column(tree, _col, False))
-
-tree.column('name', width=width_name, anchor='center')
-tree.heading('name', text='Name')
-tree.column('priority', width=width_priority, anchor='center')
-tree.heading('priority', text='Priority')
-tree.column('category', width=width_category, anchor='center')
-tree.heading('category', text='Category')
-tree.column('is_finished', width=width_is_finished, anchor='center')
-tree.heading('is_finished', text='Is Finished')
-
-scrollbar = ttk.Scrollbar(root, orient=VERTICAL, command=tree.yview)
-tree.configure(yscrollcommand=scrollbar.set)
+tree.column("is_finished", width=width_is_finished, anchor="center")
+tree.heading("is_finished", text="Is Finished")
 
 mainframe = ttk.Frame(root, padding="25 25 100 50")
 mainframe.grid(row=0, column=2, sticky=(N, S, W, E))
@@ -69,148 +72,58 @@ mainframe.rowconfigure(0, weight=1)
 mainframe.columnconfigure(0, weight=1)
 
 name = StringVar()
-priority = StringVar()
-category = StringVar()
-is_finished = BooleanVar()
-
-ttk.Label(mainframe, text='Name:').grid(column=1, row=1, sticky=(W, E))
+ttk.Label(mainframe, text="Name:").grid(column=1, row=1, sticky=(W, E))
 name_widget = ttk.Entry(mainframe, width=20, textvariable=name)
 name_widget.grid(column=2, row=1, sticky=(W, E))
 
-ttk.Label(mainframe, text='Priority:').grid(column=1, row=2, sticky=(W, E))
+priority = StringVar()
+ttk.Label(mainframe, text="Priority:").grid(column=1, row=2, sticky=(W, E))
 priority_widget = ttk.Entry(mainframe, width=20, textvariable=priority)
 priority_widget.grid(column=2, row=2, sticky=(W, E))
 
-ttk.Label(mainframe, text='Category:').grid(column=1, row=3, sticky=(W, E))
+category = StringVar()
+ttk.Label(mainframe, text="Category:").grid(column=1, row=3, sticky=(W, E))
 category_widget = ttk.Entry(mainframe, width=20, textvariable=category)
 category_widget.grid(column=2, row=3, sticky=(W, E))
 
-ttk.Label(mainframe, text='Is Finished:').grid(column=1, row=4, sticky=(W, E))
-is_finished_widget = ttk.Checkbutton(mainframe, variable=is_finished, onvalue=True, offvalue=False)
+is_finished = BooleanVar()
+ttk.Label(mainframe, text="Is Finished:").grid(column=1, row=4, sticky=(W, E))
+is_finished_widget = ttk.Checkbutton(mainframe, variable=is_finished,
+                                     onvalue=True, offvalue=False)
 is_finished_widget.grid(column=2, row=4, sticky=(W, E))
 
 
 def create_item():
-    """TODO."""
+    """Handle item creation in db and GUI."""
     name_value = name.get()
     priority_value = priority.get()
     category_value = category.get()
     is_finished_value = is_finished.get()
 
     if validate_inputs():
-        item_values = (name_value, priority_value, category_value, is_finished_value)
+        item_values = (name_value,
+                       priority_value,
+                       category_value,
+                       is_finished_value)
 
         item_id = db.add_task(item_values)
 
-        tree.insert('', 'end', item_id, text=item_id, values=(item_values[0], item_values[1], item_values[2], item_values[3]))
+        tree.insert("", "end", item_id, text=item_id, values=(item_values[0],
+                                                              item_values[1],
+                                                              item_values[2],
+                                                              item_values[3]))
 
         name.set("")
         priority.set("")
         category.set("")
-        is_finished.set('False')
+        is_finished.set(False)
 
-        create_button['state'] = 'normal'
-        change_button['state'] = 'disabled'
-
-
-def change_item():
-    """TODO."""
-    name_value = name.get()
-    priority_value = priority.get()
-    category_value = category.get()
-    is_finished_value = is_finished.get()
-
-    if validate_inputs():
-        item_id = tree.item(tree.selection()[0], "text")
-        item_values = (name_value, priority_value, category_value, is_finished_value)
-        db.edit_task(str(item_id), item_values)
-        tree.item(tree.selection()[0], values=item_values)
-
-        name.set("")
-        priority.set("")
-        category.set("")
-        is_finished.set("")
-
-        create_button['state'] = 'normal'
-        change_button['state'] = 'disabled'
-
-
-create_button = ttk.Button(mainframe, text="Create Task", command=create_item)
-create_button.grid(column=1, row=5, sticky=(W, E))
-
-change_button = ttk.Button(mainframe, text="Change Task", command=change_item)
-change_button.grid(column=2, row=5, sticky=(W, E))
-change_button['state'] = 'disabled'
-
-for child in mainframe.winfo_children():
-    child.grid_configure(padx=25, pady=25)
-
-tree.grid(row=0, column=0, rowspan=2)
-scrollbar.grid(row=0, column=1, rowspan=2, sticky=(W, N, E, S))
-
-for item in db.get_tasks():
-    tree.insert('', 'end', item[0], text=item[0], values=(item[1], item[2], item[3], item[4]))
-
-
-def remove_item_helper():
-    """TODO."""
-    item_id = tree.item(tree.selection()[0], "text")
-    db.delete_task(str(item_id))
-    tree.delete(item_id)
-
-
-def change_item_helper():
-    """TODO."""
-    item_values = tree.item(tree.selection()[0], "values")
-
-    name.set(item_values[0])
-    priority.set(item_values[1])
-    category.set(item_values[2])
-    is_finished.set(item_values[3])
-
-    create_button['state'] = 'disabled'
-    change_button['state'] = 'normal'
-
-
-menu = Menu(root, tearoff=0)
-menu.add_command(label="Remove Task", command=remove_item_helper)
-menu.add_command(label="Change Task", command=change_item_helper)
-
-
-def right_click_handler(event):
-    """TODO."""
-    show_contextual_menu(event, item)
-
-
-tree.bind("<3>", right_click_handler)
-
-
-def show_contextual_menu(event, item):
-    """TODO."""
-    if tree.focus():
-        menu.post(event.x + 65, event.y)
-
-
-def left_click_handler(event):
-    """TODO."""
-    menu.unpost()
-
-
-tree.bind("<1>", left_click_handler)
-
-
-def shutdown_hook():
-    """TODO."""
-    if messagebox.askyesno(message='Are you sure you want to quit?', icon='question', title='Quit'):
-        db.shutdown_db()
-        root.destroy()
-
-
-root.protocol("WM_DELETE_WINDOW", shutdown_hook)
+        create_button["state"] = "normal"
+        change_button["state"] = "disabled"
 
 
 def validate_inputs():
-    """TODO."""
+    """Validate inputs for item creation/editing."""
     name_value = name.get()
     priority_value = priority.get()
     category_value = category.get()
@@ -224,5 +137,115 @@ def validate_inputs():
 
     return True
 
+
+create_button = ttk.Button(mainframe, text="Create Task", command=create_item)
+create_button.grid(column=1, row=5, sticky=(W, E))
+
+
+def change_item():
+    """Handle item editing in db and GUI."""
+    name_value = name.get()
+    priority_value = priority.get()
+    category_value = category.get()
+    is_finished_value = is_finished.get()
+
+    if validate_inputs():
+        item_id = tree.item(tree.selection()[0], "text")
+        item_values = (name_value,
+                       priority_value,
+                       category_value,
+                       is_finished_value)
+
+        db.edit_task(item_id, item_values)
+        tree.item(tree.selection()[0], values=item_values)
+
+        name.set("")
+        priority.set("")
+        category.set("")
+        is_finished.set(False)
+
+        create_button["state"] = "normal"
+        change_button["state"] = "disabled"
+
+
+change_button = ttk.Button(mainframe, text="Change Task", command=change_item)
+change_button.grid(column=2, row=5, sticky=(W, E))
+change_button["state"] = "disabled"
+
+for child in mainframe.winfo_children():
+    child.grid_configure(padx=25, pady=25)
+
+for item in db.get_tasks():
+    tree.insert("", "end", item[0], text=item[0],
+                values=(item[1], item[2], item[3], item[4]))
+
+menu = Menu(root, tearoff=0)
+
+
+def remove_item():
+    """Handle item removing in db and GUI."""
+    item_id = tree.item(tree.selection()[0], "text")
+    db.delete_task(item_id)
+    tree.delete(item_id)
+
+    name.set("")
+    priority.set("")
+    category.set("")
+    is_finished.set(False)
+
+    create_button["state"] = "normal"
+    change_button["state"] = "disabled"
+
+
+menu.add_command(label="Remove Task", command=remove_item)
+
+
+def change_item_helper():
+    """Handle form controls for item editing."""
+    item_values = tree.item(tree.selection()[0], "values")
+
+    name.set(item_values[0])
+    priority.set(item_values[1])
+    category.set(item_values[2])
+    is_finished.set(item_values[3])
+
+    create_button["state"] = "disabled"
+    change_button["state"] = "normal"
+
+
+menu.add_command(label="Change Task", command=change_item_helper)
+
+
+def right_click_handler(event):
+    """Delegate to function responsible for showing the user contextual menu with further actions."""
+    show_contextual_menu(event)
+
+
+def show_contextual_menu(event):
+    """Handle popping of contextual menu."""
+    if tree.focus():
+        menu.post(event.x + 65, event.y)
+
+
+tree.bind("<3>", right_click_handler)
+
+
+def left_click_handler(event):
+    """Remove contextual menu."""
+    menu.unpost()
+
+
+tree.bind("<1>", left_click_handler)
+
+
+def shutdown_hook():
+    """Provide hook for root window shutdown event."""
+    if messagebox.askyesno(message="Are you sure you want to quit?",
+                           icon="question", title="Quit"):
+        db.shutdown_db()
+        root.destroy()
+
+
+root.protocol("WM_DELETE_WINDOW", shutdown_hook)
 
 root.mainloop()
